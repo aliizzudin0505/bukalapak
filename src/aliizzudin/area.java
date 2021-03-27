6a21398fa29475cc377b25f43532ebe6bef3052f
ali <aliiizzudin@yahoo.co.id>
2021-03-27 08:31:15 +0700

    commit1

---
+++ src/aliizzudin/area.java
@@ -0,0 +1,8 @@
+package aliizzudin;
+
+public class area {
+	public static void main(String args[]){
+		int a = 100 + 3 * 5 - 10 /5;
+		System.out.println(a);
+		}
+}
---
+++ src/aliizzudin/average.java
@@ -0,0 +1,13 @@
+package aliizzudin;
+
+public class average {
+	public static void main(String args[])
+	{
+		int a = 0;
+		for (int i=0 ; i<6 ; ++i) {
+			a = a +2;
+		}
+		System.out.println(a/6);
+	}
+	
+}
